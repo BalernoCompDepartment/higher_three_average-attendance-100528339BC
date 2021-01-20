1450257dc556aaa9f9f14288dc7fdab7a76423bb
100528339BC <100528339@schools.edin.org>
2021-01-20 14:11:48 +0000

    completed program

--- Main.java
+++ Main.java
@@ -1,18 +1,23 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-    
-  // Step 1: set total initially to 0
-    
-  //Step 2: Start a fixed loop for 8 atttendances
-    
-  //Step 3: ask user for attendance
-    
-  //Step 4: add value of attendance to total
-    
-  //Step 5: End fixed loop
-    
-  //Step 6: Round average and convert to integer
-    
-  //Step 7: display the "Average cricket attendance is " concantenated with the calue of the average and followed by " this year"\
+  Scanner scan = new Scanner(System.in);
+
+  int total;
+
+  int attendance; 
+
+  for (i = 0; i<8;i++){
+    system.out.println("how many fans attended game "+ (i + 1) + "?");
+    attendance = scan.next();
+    total = total + attendance;
+  }
+
+  double average = total/8
+
+  int averageRounded = (int) Math.round(average);
+
+   system.out.println("The Average cricket attendance is " +averageRounded+ " this year.")
+  
   }
 }
